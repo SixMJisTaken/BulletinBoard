9359e0554c195757b4bb78499abd6a5c60923f71
SixMJisTaken <myominkhant5647@gmail.com>
2024-03-11 20:39:20 +0630

    add CRUD for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,65 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def edit_confirm
+    @post = Post.new(post_params)
+    if @post.valid?
+      puts "success"
+      redirect_to confirm_posts_path(post: {title: @post.title , description: @post.description })
+    else 
+      puts "failed"
+      render :new, status: :unprocessable_entity
+    end
+    @test = "blah"
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    @testing = "success"
+  end
+
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to posts_path
+    else 
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to posts_path
+    else 
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to post_path , status: :see_other
   end
 
   def csv
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title , :description)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_19_152924) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_144115) do
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "username"
     t.string "email", default: "", null: false
